8977b50df78f1e4ab95d540b85ca3c85ce7ab50c
Fiv_ <nzaikar184@gmail.com>
2025-10-04 18:03:33 +0200

    Added new post controller

--- src/main/java/edu/team/carshopbackend/controller/ApiController.java
+++ src/main/java/edu/team/carshopbackend/controller/ApiController.java
@@ -1,8 +1,6 @@
 package edu.team.carshopbackend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/")
@@ -11,6 +9,11 @@ public class ApiController {
     @GetMapping("hello_world")
     public String helloWorld() {
         return "Hello World!";
+    }
+
+    @PostMapping("test_post/{id}")
+    public Integer post(@PathVariable("id") Integer id) {
+        return id;
     }
 
 }
