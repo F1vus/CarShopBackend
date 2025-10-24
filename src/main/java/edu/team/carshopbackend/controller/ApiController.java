ad48adba6aff961964bc73d96113d8b0fa265cc7
RealMan <maxim27112711@gmail.com>
2025-10-24 19:43:44 +0200

    I added Swagger to Spring Boot project

--- src/main/java/edu/team/carshopbackend/controller/ApiController.java
+++ src/main/java/edu/team/carshopbackend/controller/ApiController.java
@@ -1,17 +1,22 @@
 package edu.team.carshopbackend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/")
+@Tag(name = "API Test", description = "Przykładowe endpointy testowe")
 public class ApiController {
 
     @GetMapping("hello_world")
+    @Operation(summary = "Hello World", description = "Zwraca prosty komunikat Hello World")
     public String helloWorld() {
         return "Hello World!";
     }
 
     @PostMapping("test_post/{id}")
+    @Operation(summary = "Test POST", description = "Zwraca przekazane ID w ścieżce")
     public Integer post(@PathVariable("id") Integer id) {
         return id;
     }
