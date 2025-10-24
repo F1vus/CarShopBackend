bca8799cd98beb8d186ecde6765600df8f8ba68b
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2025-10-24 22:29:14 +0200

    add POST,PATCH,DELETE

--- src/main/java/edu/team/carshopbackend/controller/CarController.java
+++ src/main/java/edu/team/carshopbackend/controller/CarController.java
@@ -19,6 +19,13 @@ public class CarController {
         this.carService = carService;
     }
 
+    @PostMapping("/c")
+    public CarDTO createCar(@RequestBody CarDTO carDTO) {
+       Car savedCar = carService.createProduct(CarMapper.mapToEntity(carDTO));
+       return CarMapper.mapToDTO(savedCar);
+    }
+
+
     @GetMapping("/cars")
     public List<CarDTO> findAllAutos() {
         return carService.getAllProducts().stream()
@@ -29,9 +36,42 @@ public class CarController {
     @GetMapping("/cars/{id}")
     public CarDTO findAutoById(@PathVariable Long id) {
         Car car = carService.getProductById(id)
-                .orElseThrow(() -> new RuntimeException("No Car found with id " + id));
+                .orElseThrow(() -> new RuntimeException("Car not found with id " + id));
         return CarMapper.mapToDTO(car);
     }
+
+
+    @PatchMapping("/changeCarAdress/{id}")
+    public CarDTO updateCar(@PathVariable Long id, @RequestBody CarDTO carDTO) {
+        Car existingCar = carService.getProductById(id)
+                .orElseThrow(() -> new RuntimeException("Car not found with id " + id));
+
+        if (carDTO.getName() != null) existingCar.setName(carDTO.getName());
+        if (carDTO.getPrice() != null) existingCar.setPrice(carDTO.getPrice());
+        if (carDTO.getDescription() != null) existingCar.setDescription(carDTO.getDescription());
+        if(carDTO.getMileage() != null) existingCar.setMileage(carDTO.getMileage());
+        if(carDTO.getState()!= null) existingCar.setCar_status(carDTO.getState());
+        if(carDTO.getEngineCapacity() != null) existingCar.setEngine_capacity(carDTO.getEngineCapacity());
+        if(carDTO.getPower() != null) existingCar.setPower(carDTO.getPower());
+        if(carDTO.getYear() != null) existingCar.setYear(carDTO.getYear());
+        if(carDTO.getId() != null) existingCar.setId(carDTO.getId());
+
+        Car savedCar = carService.updateProduct(existingCar);
+        return CarMapper.mapToDTO(savedCar);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteCar(@PathVariable Long id) {
+        carService.deleteCarById(id);
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteAllCars() {
+        carService.deleteAllData();
+    }
+
+
+
 
 
 
--- src/main/java/edu/team/carshopbackend/mapper/CarMapper.java
+++ src/main/java/edu/team/carshopbackend/mapper/CarMapper.java
@@ -17,15 +17,39 @@ public class CarMapper {
                 .name(car.getName())
                 .price(car.getPrice())
                 .description(car.getDescription())
-                .color(car.getColor().getName())
+                .color(car.getColor() != null ? car.getColor().getName() : null)
                 .mileage(car.getMileage())
                 .state(car.getCar_status())
-                .petrolType(car.getPetrolType().getName())
+                .petrolType(car.getPetrolType() != null ? car.getPetrolType().getName() : null)
                 .engineCapacity(car.getEngine_capacity())
                 .power(car.getPower())
                 .year(car.getYear())
                 .imageUrl(car.getImageUrl())
                 .build();
+    }
+
+
+    public static Car mapToEntity(CarDTO carDTO) {
+        if (carDTO == null) {
+            return null;
+        }
+
+        Car car = new Car();
+        //car.setId(dto.getId());
+        car.setName(carDTO.getName());
+        car.setPrice(carDTO.getPrice());
+        car.setDescription(carDTO.getDescription());
+//    car.setColor(dto.getColor());
+        car.setMileage(carDTO.getMileage());
+        car.setCar_status(carDTO.getState());
+//    car.setPetrolType(dto.getPetrolType());
+        car.setEngine_capacity(carDTO.getEngineCapacity());
+        car.setPower(carDTO.getPower());
+        car.setYear(carDTO.getYear());
+        car.setImageUrl(carDTO.getImageUrl());
+
+        return car;
+
     }
 
 }
