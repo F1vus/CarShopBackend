ad2d84f6678531dcc34d07a5761641e9298bb9a6
Fiv_ <nzaikar184@gmail.com>
2025-10-30 18:15:56 +0100

    Changed spring SecurityConfig.java

--- src/main/java/edu/team/carshopbackend/config/SecurityConfig.java
+++ src/main/java/edu/team/carshopbackend/config/SecurityConfig.java
@@ -6,19 +6,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                .build();
+    protected SecurityFilterChain configure(final HttpSecurity security) throws Exception
+    {
+        return security.csrf(AbstractHttpConfigurer::disable).cors(
+                httpSecurityCorsConfigurer ->
+                        httpSecurityCorsConfigurer.configurationSource(request ->
+                                new CorsConfiguration().applyPermitDefaultValues())
+
+        ).build();
     }
 }
