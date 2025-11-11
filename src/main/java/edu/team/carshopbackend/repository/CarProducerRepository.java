5c15c3723553e384e979df1944fa1c7b6106d119
Fiv_ <nzaikar184@gmail.com>
2025-11-11 20:45:19 +0100

    Minor fix

--- src/main/java/edu/team/carshopbackend/repository/CarProducerRepository.java
+++ src/main/java/edu/team/carshopbackend/repository/CarProducerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarProducerRepository extends JpaRepository<Long, CarProducent> {
+public interface CarProducerRepository extends JpaRepository<CarProducent, Long> {
 }
