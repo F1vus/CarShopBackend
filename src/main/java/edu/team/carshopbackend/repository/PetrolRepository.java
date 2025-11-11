1995a50116eb30f3b787009f6d46ddcd8bfee0ca
Fiv_ <nzaikar184@gmail.com>
2025-11-11 20:42:52 +0100

    Added @Repository annotation to new repositories

--- src/main/java/edu/team/carshopbackend/repository/PetrolRepository.java
+++ src/main/java/edu/team/carshopbackend/repository/PetrolRepository.java
@@ -2,6 +2,8 @@ package edu.team.carshopbackend.repository;
 
 import edu.team.carshopbackend.entity.Petrol;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PetrolRepository extends JpaRepository<Petrol, Long> {
 }
