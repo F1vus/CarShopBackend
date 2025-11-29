d79e7ee22b966fd5e05bd2aa4d315b84574c28b3
RealMan <maxim27112711@gmail.com>
2025-11-30 00:27:44 +0100

    fix

--- src/main/java/edu/team/carshopbackend/dto/AuthDTO/ProfileDTO.java
+++ src/main/java/edu/team/carshopbackend/dto/AuthDTO/ProfileDTO.java
@@ -1,7 +1,7 @@
 package edu.team.carshopbackend.dto.AuthDTO;
 
-
 import edu.team.carshopbackend.entity.Profile;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +10,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProfileDTO {
+
+    @Schema(description = "ID profilu")
     private Long id;
+
+    @Schema(description = "Nazwa profilu")
     private String name;
+
+    @Schema(description = "Telefon użytkownika")
+    private String phoneNumber;
+
+    @Schema(description = "Email użytkownika")
+    private String email;
+
+    @Schema(description = "URL zdjęcia profilowego")
+    private String profileImage;
 
     public ProfileDTO(Profile profile) {
         this.id = profile.getId();
