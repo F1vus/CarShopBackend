8c8aca0af5a4a46080a237d545fbce8f7bf6b41d
RealMan <maxim27112711@gmail.com>
2025-12-07 17:21:53 +0100

    I added to the Car entity the options with accidents/without accidents

--- src/main/java/edu/team/carshopbackend/mapper/impl/CarMapper.java
+++ src/main/java/edu/team/carshopbackend/mapper/impl/CarMapper.java
@@ -10,17 +10,23 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class CarMapper implements Mapper<Car, CarDTO> {
+
     private final ModelMapper modelMapper;
 
     @Override
     public CarDTO mapTo(Car car) {
         CarDTO carDTO = modelMapper.map(car, CarDTO.class);
-        carDTO.getOwner().setEmail(car.getOwner().getUser().getEmail());
+        if (car.getOwner() != null && car.getOwner().getUser() != null) {
+            carDTO.getOwner().setEmail(car.getOwner().getUser().getEmail());
+        }
+        carDTO.setHadAccidents(car.getHadAccidents());
         return carDTO;
     }
 
     @Override
     public Car mapFrom(CarDTO carDTO) {
-        return modelMapper.map(carDTO, Car.class);
+        Car car = modelMapper.map(carDTO, Car.class);
+        car.setHadAccidents(carDTO.getHadAccidents());
+        return car;
     }
 }
