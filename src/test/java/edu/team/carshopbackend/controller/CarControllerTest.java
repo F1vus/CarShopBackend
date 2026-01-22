e454b784000df98b7e6399c434e564f250265354
Kostiantyn Kolesnyk <kostiakolesnik2513@gmail.com>
2026-01-22 22:40:38 +0100

    fix test

--- src/test/java/edu/team/carshopbackend/controller/CarControllerTest.java
+++ src/test/java/edu/team/carshopbackend/controller/CarControllerTest.java
@@ -3,8 +3,11 @@ package edu.team.carshopbackend.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.team.carshopbackend.config.jwtConfig.JwtCore;
 import edu.team.carshopbackend.dto.CarDTO;
+import edu.team.carshopbackend.dto.CreateCarRequestDTO;
 import edu.team.carshopbackend.entity.Car;
 import edu.team.carshopbackend.entity.Photo;
+import edu.team.carshopbackend.entity.Profile;
+import edu.team.carshopbackend.entity.impl.UserDetailsImpl;
 import edu.team.carshopbackend.mapper.impl.CarMapper;
 import edu.team.carshopbackend.repository.JwtTokenRepository;
 import edu.team.carshopbackend.service.CarService;
@@ -15,6 +18,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -26,8 +35,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -57,31 +65,43 @@ class CarControllerTest {
 
     @Test
     void shouldCreateCarSuccessfully() throws Exception {
-        CarDTO requestDto = new CarDTO();
+
+        Profile profile = new Profile();
+
+        UserDetailsImpl userDetails = mock(UserDetailsImpl.class);
+        when(userDetails.getProfile()).thenReturn(profile);
+        when(userDetails.getAuthorities()).thenReturn(List.of());
+
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+
+        CreateCarRequestDTO requestDto = new CreateCarRequestDTO();
         requestDto.setName("Audi");
-
-        Car entity = new Car();
-        entity.setName("Audi");
-
-        Car savedEntity = new Car();
-        savedEntity.setId(1L);
-        savedEntity.setName("Audi");
 
         CarDTO responseDto = new CarDTO();
         responseDto.setId(1L);
         responseDto.setName("Audi");
 
-        when(carMapper.mapFrom(requestDto)).thenReturn(entity);
-        when(carService.createProduct(entity)).thenReturn(savedEntity);
-        when(carMapper.mapTo(savedEntity)).thenReturn(responseDto);
+        when(carService.createCarWithPhotos(any(), any(), eq(profile)))
+                .thenReturn(responseDto);
 
-        mockMvc.perform(post("/api/v1/cars")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(requestDto)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(1)))
-                .andExpect(jsonPath("$.name", is("Audi")));
+        MockMultipartFile carPart = new MockMultipartFile(
+                "car",
+                "car.json",
+                "application/json",
+                objectMapper.writeValueAsBytes(requestDto)
+        );
+
+        mockMvc.perform(multipart("/api/v1/cars")
+                        .file(carPart))
+                .andExpect(status().isOk());
     }
+
+
 
     @Test
     void shouldUpdateCarSuccessfully() throws Exception {
